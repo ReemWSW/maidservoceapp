3624e06ff58adf4c4ec8f80343160b261a91577f
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-19 11:22:31 +0700

    pass function to text buttom

--- lib/widget/text_button.dart
+++ lib/widget/text_button.dart
@@ -4,15 +4,18 @@ class ButtonTextCustom extends StatelessWidget {
   const ButtonTextCustom({
     Key? key,
     required this.label,
+    required this.onPressed,
   }) : super(key: key);
 
   final String label;
+  final VoidCallback onPressed;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
       child: TextButton(
-          onPressed: () {},
+          onPressed: onPressed,
           child: Text(
             label,
             style: const TextStyle(color: Colors.green, fontSize: 16),
