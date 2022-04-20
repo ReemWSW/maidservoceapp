6279978622e248108668b92941a6cca9cc1eee68
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-20 09:59:54 +0700

    url api

---
+++ lib/utils/app_url.dart
@@ -0,0 +1,8 @@
+class AppUrl {
+  // static const String liveBaseURL = "https://miad-app.herokuapp.com";
+  static const String localBaseURL = "http://10.0.2.2:3000";
+
+  static const String baseURL = localBaseURL;
+  static const String login = baseURL + "/login";
+  static const String register = baseURL + "/register";
+}
