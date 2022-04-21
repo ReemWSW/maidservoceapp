45ff614cbb17c0960c25a012b564cbba1ca7fb25
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-21 16:52:52 +0700

    bottom bar

---
+++ lib/pages/home/bottombar.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'home.dart';
+import 'texttest.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Home(),
+    NewWidget(
+      optionStyle: optionStyle,
+      label: '2',
+    ),
+    NewWidget(
+      optionStyle: optionStyle,
+      label: '3',
+    ),
+    NewWidget(
+      optionStyle: optionStyle,
+      label: '4',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'หน้าแรก',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'รายการ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.create_rounded),
+            label: 'งานของฉัน',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'ฉัน',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.lightGreenAccent,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
