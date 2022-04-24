ca323f1943529dde532e14a3a47cd4f3ac639bcb
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-24 15:32:30 +0700

    order models

---
+++ lib/models/order.dart
@@ -0,0 +1,95 @@
+class Order {
+  Customer? customer;
+  Customer? maid;
+  Address? address;
+  String? categoty;
+  String? type;
+  String? detail;
+  String? datetime;
+
+  Order(
+      {this.customer,
+      this.maid,
+      this.address,
+      this.categoty,
+      this.type,
+      this.detail,
+      this.datetime});
+
+  Order.fromJson(Map<String, dynamic> json) {
+    customer =
+        json['customer'] != null ? Customer.fromJson(json['customer']) : null;
+    maid = json['maid'] != null ? Customer.fromJson(json['maid']) : null;
+    address =
+        json['address'] != null ? Address.fromJson(json['address']) : null;
+    categoty = json['categoty'];
+    type = json['type'];
+    detail = json['detail'];
+    datetime = json['datetime'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (customer != null) {
+      data['customer'] = customer!.toJson();
+    }
+    if (maid != null) {
+      data['maid'] = maid!.toJson();
+    }
+    if (address != null) {
+      data['address'] = address!.toJson();
+    }
+    data['categoty'] = categoty;
+    data['type'] = type;
+    data['detail'] = detail;
+    data['datetime'] = datetime;
+    return data;
+  }
+}
+
+class Customer {
+  String? image;
+  String? name;
+  String? phone;
+
+  Customer({this.image, this.name, this.phone});
+
+  Customer.fromJson(Map<String, dynamic> json) {
+    image = json['image'];
+    name = json['name'];
+    phone = json['phone'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['image'] = image;
+    data['name'] = name;
+    data['phone'] = phone;
+    return data;
+  }
+}
+
+class Address {
+  String? detail;
+  String? tombon;
+  String? amphure;
+  String? province;
+
+  Address({this.detail, this.tombon, this.amphure, this.province});
+
+  Address.fromJson(Map<String, dynamic> json) {
+    detail = json['detail'];
+    tombon = json['tombon'];
+    amphure = json['amphure'];
+    province = json['province'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['detail'] = detail;
+    data['tombon'] = tombon;
+    data['amphure'] = amphure;
+    data['province'] = province;
+    return data;
+  }
+}
