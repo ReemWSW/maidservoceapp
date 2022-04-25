5e891226b8e6bc7319d9baf1911e433a4de7e41c
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-25 08:26:24 +0700

    [profile] split widget image and text

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:maid/utils/sharepreferences/auth.dart';
+
+import 'widgets/container_image.dart';
+import 'widgets/row_text.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({
+  ProfilePage({
     Key? key,
   }) : super(key: key);
 
+  String? _image, _name, _phone, _email;
+
   @override
   Widget build(BuildContext context) {
+    UserPreferences().getUser().then((user) {
+      _image = user.image;
+      _name = user.name;
+      _phone = user.phone;
+      _email = user.email;
+    });
     return Container(
       margin: const EdgeInsets.all(16),
       child: Column(
         children: [
-          Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 7,
-                  spreadRadius: 3,
-                ),
-              ],
-            ),
-            child: const Icon(Icons.account_circle_outlined),
-          )
+          ContainerImage(image: _image),
+          RowText(label: 'ชื่อ', text: '$_name'),
+          RowText(label: 'อีเมลล์', text: '$_email'),
+          RowText(label: 'เบอร์โทรศัพท์', text: '$_phone'),
         ],
       ),
     );
