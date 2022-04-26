dd72f53f53cc9f1fbb06ea56304064070729ac55
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-26 15:31:40 +0700

    [list] show data in review

--- lib/pages/list/widgets/review.dart
+++ lib/pages/list/widgets/review.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:maid/providers/order_provider.dart';
+import 'package:provider/provider.dart';
 
 class ReviewWidget extends StatelessWidget {
   const ReviewWidget({
@@ -32,70 +34,60 @@ class ReviewWidget extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 ListTile(
-                  leading: const CircleAvatar(
-                    child: Icon(Icons.people),
-                  ),
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("${order[index]['customer']['name']}"),
-                      Text("${order[index]['customer']['phone']}"),
-                    ],
-                  ),
-                  subtitle: Text(DateFormat('วันที่ yyyy-MM-dd เวลา kk:mm')
-                      .format(DateTime.parse(order[index]['datetime']))),
-                  isThreeLine: true,
-                  // trailing: order[index]['score'] != 0
-                  //     ? CircleAvatar(
-                  //         backgroundColor: Colors.white,
-                  //         child: Text("${order[0]["score"]}"),
-                  //       )
-                  //     : ElevatedButton(
-                  //         onPressed: () {
-                  //           showDialog<String>(
-                  //             context: context,
-                  //             builder:
-                  //                 (BuildContext context) =>
-                  //                     AlertDialog(
-                  //               title: const Text('คะแนน'),
-                  //               content: Row(
-                  //                   children:
-                  //                       List.generate(
-                  //                               6,
-                  //                               (int index) =>
-                  //                                   index++)
-                  //                           .map(
-                  //                               (score) =>
-                  //                                   Flexible(
-                  //                                     flex: 1,
-                  //                                     fit: FlexFit
-                  //                                         .loose,
-                  //                                     child:
-                  //                                         Container(
-                  //                                       margin:
-                  //                                           const EdgeInsets.all(6),
-                  //                                       child: ElevatedButton(
-                  //                                           onPressed: () {
-                  //                                             setState(() {
-                  //                                               Provider.of<OrderProvider>(context, listen: false).setScoreOrder(order[0]["_id"], score);
-                  //                                               Navigator.pop(context);
-                  //                                             });
-                  //                                           },
-                  //                                           child: Text('$score')),
-                  //                                     ),
-                  //                                   ))
-                  //                           .toList()),
-                  //             ),
-                  //           );
-                  //         },
-                  // child: Text(order[index]['score'] ==
-                  //         0
-                  //     ? 'ให้คะแนน'
-                  //     : '${order[index]['score']}'))
-                ),
+                    leading: const CircleAvatar(
+                      child: Icon(Icons.people),
+                    ),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("${order[index]['customer']['name']}"),
+                        Text("${order[index]['customer']['phone']}"),
+                      ],
+                    ),
+                    subtitle: Text(DateFormat('วันที่ yyyy-MM-dd เวลา kk:mm')
+                        .format(DateTime.parse(order[index]['datetime']))),
+                    isThreeLine: true,
+                    trailing: order[index]['score'] != 0
+                        ? CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: Text("${order[0]["score"]}"),
+                          )
+                        : ElevatedButton(
+                            onPressed: () {
+                              generateScore(context, index);
+                            },
+                            child: Text(order[index]['score'] == 0
+                                ? 'ให้คะแนน'
+                                : '${order[index]['score']}'))),
               ],
             ),
           );
         });
+  }
+
+  Future<String?> generateScore(BuildContext context, int index) {
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('คะแนน'),
+        content: Row(
+            children: List.generate(6, (int index) => index++)
+                .map((score) => Flexible(
+                      flex: 1,
+                      fit: FlexFit.loose,
+                      child: Container(
+                        margin: const EdgeInsets.all(6),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              Provider.of<OrderProvider>(context, listen: false)
+                                  .setScoreOrder(order[index]["_id"], score);
+                              Navigator.pop(context);
+                            },
+                            child: Text('$score')),
+                      ),
+                    ))
+                .toList()),
+      ),
+    );
   }
 }
