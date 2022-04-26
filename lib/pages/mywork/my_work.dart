17c5225c44d8b59770bc32b4b2c6095ca30df918
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-26 15:04:38 +0700

    [mywork] work info

--- lib/pages/mywork/my_work.dart
+++ lib/pages/mywork/my_work.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:maid/providers/order_provider.dart';
+import 'package:maid/utils/sharepreferences/auth.dart';
+import 'package:provider/provider.dart';
 
 import 'widgets/list_work.dart';
 import 'widgets/review_work.dart';
@@ -13,39 +16,60 @@ class MyWorkPage extends StatefulWidget {
 }
 
 class _MyWorkPageState extends State<MyWorkPage> {
-  // List<String> name = ['รับงาน', 'รับงานแล้ว', 'รีวิว'];
-  List<String> name = ['รายการปัจจุบัน', 'รายการที่ผ่าน', 'รีวิว'];
+  List<String> name = ['รับงาน', 'รับงานแล้ว', 'รีวิว'];
+  String? _idMaid;
+
+  void fetchData() async {
+    await UserPreferences().getUser().then((user) {
+      _idMaid = user.id;
+    });
+    await Provider.of<OrderProvider>(context, listen: false)
+        .getorderCustomer(_idMaid!, false);
+  }
+
+  @override
+  void initState() {
+    fetchData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.green,
-          elevation: 0,
-          bottom: TabBar(
-              labelColor: Colors.green,
-              unselectedLabelColor: Colors.white,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicator: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10)),
-                  color: Colors.white),
-              tabs: name
-                  .map((label) => Tab(
-                      child: Align(
-                          alignment: Alignment.center, child: Text(label))))
-                  .toList()),
-        ),
-        body: const TabBarView(children: [
-          ListWork(),
-          ListWork(),
-          ReviewWork(),
-        ]),
-      ),
-    );
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.green,
+            elevation: 0,
+            bottom: TabBar(
+                labelColor: Colors.green,
+                unselectedLabelColor: Colors.white,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicator: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    color: Colors.white),
+                tabs: name
+                    .map((label) => Tab(
+                        child: Align(
+                            alignment: Alignment.center, child: Text(label))))
+                    .toList()),
+          ),
+          body: Consumer<OrderProvider>(builder: (context, order, child) {
+            return TabBarView(children: [
+              ListWork(
+                booking: true,
+                order: order.waitOrder!,
+              ),
+              ListWork(
+                booking: true,
+                order: order.waitOrder!,
+              ),
+              ReviewWork(),
+            ]);
+          }),
+        ));
   }
 }
---
+++ lib/pages/mywork/my_work_info.dart
@@ -0,0 +1,126 @@
+// ignore_for_file: unrelated_type_equality_checks
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class MyWorkInfo extends StatelessWidget {
+  const MyWorkInfo({
+    Key? key,
+    required this.booking,
+    required this.order,
+    required this.index,
+  }) : super(key: key);
+
+  final bool booking;
+  final List order;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    const textheadStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('รายละเอียดงาน'),
+        ),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text("${order[index]["customer"]["name"]}",
+                      style: const TextStyle(fontSize: 20)),
+                  Text('เบอร์โทรศัพท์ ${order[index]["customer"]["phone"]}',
+                      style: const TextStyle(fontSize: 20)),
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Card(
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'รายละเอียดของงาน',
+                                  style: textheadStyle,
+                                ),
+                                if (order[index]["category"] ==
+                                    "Categories.CLEANING")
+                                  textData("ทำความสะอาดบ้าน")
+                                else if (order[index]["category"] ==
+                                    "Categories.FURNITURE")
+                                  textData("ทำความสะอาดเฟอร์นิดจอร์")
+                                else if (order[index]["category"] ==
+                                    "Categories.WASH")
+                                  textData("ซักผ้า"),
+                                textData(order[index]["type"]),
+                                textData(DateFormat('วันที่ yyyy-MM-dd').format(
+                                    DateTime.parse(order[index]['datetime']))),
+                                textData(DateFormat('เวลา kk:mm').format(
+                                    DateTime.parse(order[index]['datetime']))),
+                                const SizedBox(
+                                  height: 50,
+                                ),
+                                const Text('ที่อย่', style: textheadStyle),
+                                textData(
+                                    'ที่อยู่ ${order[index]["address"]["detail"]} ตำบล ${order[index]["address"]["tombon"]} อำเภอ ${order[index]["address"]["amphure"]} จังหวัด ${order[index]["address"]["province"]}'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            booking
+                ? Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      margin: const EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.green),
+                              onPressed: () async {
+                                // await Provider.of<OrderProvider>(context,
+                                //         listen: false)
+                                //     .setStatusOrder(widget.order["_id"],
+                                //         StatusOrder.ACCEPT);
+                                // Navigator.of(context).pushAndRemoveUntil(
+                                //     MaterialPageRoute(
+                                //         builder: (context) =>
+                                //             const GetListJobPage()),
+                                //     (Route<dynamic> route) => false);
+                              },
+                              child: const Text('รับงาน'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : const SizedBox()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Text textData(String text) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 16),
+    );
+  }
+}
