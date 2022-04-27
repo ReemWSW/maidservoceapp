71afcd9e2d2a1fce336aaec9f988d1a735cbfc33
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-27 11:37:16 +0700

    [profile] setting profile

--- lib/pages/profile/maid.dart
+++ lib/pages/profile/maid.dart
@@ -9,11 +9,28 @@ class MaidSetupPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ตั้งค่าแม่บ้าน'),
       ),
-      body: Column(
-        children: const [
-          Text('data'),
-          Text('data'),
-        ],
+      body: Container(
+        margin: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const Text(
+              'บริเวณที่ต้องการทำงาน',
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(6),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                      color: Colors.grey, width: 2, style: BorderStyle.solid)),
+              child: const Text(
+                '',
+                style: TextStyle(fontSize: 16),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
